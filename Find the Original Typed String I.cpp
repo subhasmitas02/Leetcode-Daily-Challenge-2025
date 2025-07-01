fc4cd1287289b54ef5642e0d684bf94e5e72eddc
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-07-01 20:57:51 +0530

    Solved Find the Original Typed String I.cpp

---
+++ Find the Original Typed String I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        long long ans = 1;                          // original
+        for (int i = 0, n = word.size(); i < n; ) {
+            int j = i;
+            while (j < n && word[j] == word[i]) ++j; // same‑char block
+            ans += (j - i - 1);                     // add 
+            i = j;
+        }
+        return static_cast<int>(ans);
+    }
+};
