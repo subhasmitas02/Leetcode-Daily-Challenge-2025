1747bbece8ee7a853a0d2bdc8725a373937e8050
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-09-25 16:34:08 +0530

    Solved Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -26,3 +26,55 @@ public:
 
 //TC -> O(n*n)
 //SC -> O(n) 
+
+
+// 3) Tabulation Approach --------------------
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int row=triangle.size();
+//         vector<vector<int>> dp(row, vector<int>(row,0));
+
+//         for(int col=0;col<row;col++)
+//         {
+//             dp[row-1][col] = triangle[row-1][col];
+//         }
+//         for(int i=row-2; i>=0; i--)
+//         {
+//             for(int j=i;j>=0;j--)
+//             {
+//                 int down = dp[i+1][j];
+//                 int diagonal = dp[i+1][j+1];
+//                 dp[i][j] = triangle[i][j] + min(down, diagonal);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+//TC -> O(n*n)
+//SC -> O(n*n) for 2D DP matrix
+
+//   2)Memoization------------------
+//     int f(int i, int j, int sum, vector<vector<int>>& triangle, vector<vector<int>> &dp)
+//     {
+//         int n=triangle.size();
+//         if(i==n-1) return triangle[n-1][j];
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int down = f(i+1, j, sum, triangle, dp);
+//         int diagonal = f(i+1, j+1, sum, triangle, dp);
+//         sum = triangle[i][j] + min(down, diagonal);
+
+//         return dp[i][j]=sum;
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int row=triangle.size(), sum=0;
+//         vector<vector<int>> dp(row, vector<int>(row,-1));
+//         return f(0,0,sum, triangle, dp);
+//     }
+// };
+
+// //TC -> O(n*n)
+// //SC -> O(n*n) for 2d dp matrix + O(n) for recursion stack space
