dfdf342b9988774341e085079dd1c24213c0e10f
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-11-11 10:20:11 +0530

    Solved Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), ind = -1;
+
+        for(int i=n-2; i>=0; i--)
+        {
+            if(nums[i+1] > nums[i])
+            {
+                ind = i;
+                break;
+            }
+        }
+
+        // edge case handling
+        if(ind == -1)
+            reverse(nums.begin(), nums.end());
+
+        else
+        {
+            for(int i = n-1; i>=0; i--)
+            {
+                if(nums[i] > nums[ind])
+                {
+                    swap(nums[i], nums[ind]);
+                    break;
+                }
+            }
+            reverse(nums.begin()+ind+1, nums.end());
+        }
+        
+    }
+};
